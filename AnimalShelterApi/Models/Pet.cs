c4580814ac77adb9808b7550a9f1ad8c973d37dd
Kyle Chinick <kylechinick@icloud.com>
2022-08-19 10:20:34 -0700

    WIP get endpoint to load

--- AnimalShelterApi/Models/Pet.cs
+++ AnimalShelterApi/Models/Pet.cs
@@ -11,6 +11,7 @@ namespace AnimalShelterApi.Models
     [Required]
     public string Species { get; set; }
     [Required]
+    [Range(0, 200, ErrorMessage = "Age must be between 0 and 200.")]
     public int Age { get; set; }
     [Required]
     public string Gender { get; set; }
