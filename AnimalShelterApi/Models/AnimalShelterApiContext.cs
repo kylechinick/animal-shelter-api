96872216234dfc5c3f82f97cd6267522b269b3a5
Kyle Chinick <kylechinick@icloud.com>
2022-08-19 17:40:38 -0700

    Correct typos

--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -14,11 +14,11 @@ namespace AnimalShelterApi.Models
       builder.Entity<Animal>()
         .HasData(
             new Animal { AnimalId = 1, Name = "Albert", Species = "Dog", Age = 2, Gender = "Male", Bio = "Albert likes long walks on the beach. He also likes long walks absolutely everywhere else too." },
-            new Animal { AnimalId = 2, Name = "Bob", Species = "Panda", Age = 8, Gender = "Unknown", Bio = "Bob is a very friendly dog but has a strange coat for a dog and is unusually large. Big fan of bamboo though." },
+            new Animal { AnimalId = 2, Name = "Bob", Species = "Panda", Age = 8, Gender = "Unknown", Bio = "Bob is very friendly but has a strange coat for a dog and is unusually large. Big fan of bamboo though." },
             new Animal { AnimalId = 3, Name = "Charlie", Species = "Cat", Age = 8, Gender = "Female", Bio = "Charlie's hobbies include sleeping, eating, asking for head-pats and promptly walking away when you offer them." },
-            new Animal { AnimalId = 4, Name = "Diana", Species = "Capybara", Age = 64, Gender = "Female", Bio = "Wanted by several international policing agencies for arson. Do no allow near matches unsupervised. Likes Cheese Puffs." },
+            new Animal { AnimalId = 4, Name = "Diana", Species = "Capybara", Age = 64, Gender = "Female", Bio = "Wanted by several international policing agencies for arson. Do not allow near matches unsupervised. Likes Cheese Puffs." },
             new Animal { AnimalId = 5, Name = "Greggory", Species = "Beholder", Age = 506, Gender = "Unknown", Bio = "Despite Greggory's unique appearance he makes fast friends and loves playing fetch. Very difficult to sneak up on though so hide-and-seek is probably out of the question." },
-            new Animal { AnimalId = 6, Name = "Turbo", Species = "Narwhal", Age = 32, Gender = "Male", Bio = "Pure speed." },
+            new Animal { AnimalId = 6, Name = "Turbo", Species = "Narwhal", Age = 32, Gender = "Male", Bio = "Pure speed. Ka-chow." },
             new Animal { AnimalId = 7, Name = "Lazer", Species = "Narwhal", Age = 28, Gender = "Male", Bio = "Tried out for the Globo Gym Purple Cobras and made the team" }
         );
     }
